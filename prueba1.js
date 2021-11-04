6fdb5dbb7379a2c976a6c012c4f6cd196a819d7e
RodrigoMolinaMadrid <rodrigomolina@doofinder.com>
2021-11-04 19:07:55 +0100

    Intento 2 de funcionamiento de las ssh keys

--- prueba1.js
+++ prueba1.js
@@ -1,5 +1,5 @@
 function suma(a,b){
-  return console.log("El resultado de la suma de " + a + " + " + b + " es: " + (a + b));
+  return console.log("El resultado de las suma de " + a + " + " + b + " es: " + (a + b));
 }
 function resta(a,b){
   return console.log("El resultado de la resta de" , a , "-" , b , "es:", a - b);
